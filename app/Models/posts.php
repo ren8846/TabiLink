e879fc60a741a20c7b000ca0a0a85c09034615db
komatsu-haruka <koma.2.halca@icloud.com>
2025-08-15 11:36:04 +0900

    migrationファイル作成

---
+++ app/Models/posts.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Post extends Model
+{
+    protected $table = 'posts';
+    protected $fillable = ['title','body']; // マイグレーションのカラム名と一致させる
+    // テーブルに timestamps が無いなら:
+    // public $timestamps = false;
+}
+
