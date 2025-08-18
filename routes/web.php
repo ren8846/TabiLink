74e3854fc5e51b1428923c685910a1699a89f970
komatsu-haruka <koma.2.halca@icloud.com>
2025-08-18 10:23:33 +0900

    投稿画面ルーティング

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\PostController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -16,6 +17,9 @@ Route::middleware('auth')->group(function () {
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
+
+Route::get('/posts/create', [PostController::class, 'create'])->name('posts.create'); // 新規投稿画面
+Route::post('/posts', [PostController::class, 'store'])->name('posts.store');        
 
 Route::get('/whoami', fn() => ['auth' => auth()->check(), 'id' => optional(auth()->user())->id]);
 
